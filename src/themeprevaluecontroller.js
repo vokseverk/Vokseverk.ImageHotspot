06830a713825c3a18068030d4d351b9293860f6b
Chriztian Steinmeier <chriztian@steinmeier.dk>
2016-04-24 00:46:43 +0200

    Add prevalue editor that works

---
+++ src/themeprevaluecontroller.js
@@ -0,0 +1,8 @@
+angular.module("umbraco").controller("ImageHotspotThemePrevalueController", function ($scope) {
+	$scope.model.prevalues = [
+		{ id: 1, label: "Theme 1" },
+		{ id: 2, label: "Theme 2" },
+		{ id: 3, label: "Theme 3" },
+		{ id: 4, label: "Theme 4" }
+	];
+});
