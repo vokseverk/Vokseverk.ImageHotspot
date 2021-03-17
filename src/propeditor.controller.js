e3258f294bacc5bdc998c45fd6cad5e77bbeb148
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-03-17 11:30:10 +0100

    (Formatting)

--- src/propeditor.controller.js
+++ src/propeditor.controller.js
@@ -1,18 +1,17 @@
 angular.module("umbraco").controller("ImageHotspotController", function($scope, $element, mediaResource) {
-
 	$scope.image = {
 		src: "",
 		width: 400,
 		height: 0
-	};
-
+	}
+	
 	$scope.setImageSrc = function(context, propertyAlias) {
-		var imageRef = "";
-		var maxRecurse = 200;
-		var found = false;
-		var aliasRE = new RegExp(`${propertyAlias}\$`);
-		var ref;
-
+		var imageRef = ""
+		var maxRecurse = 200
+		var found = false
+		var aliasRE = new RegExp(`${propertyAlias}\$`)
+		var ref
+		
 		while (!found && maxRecurse > 0) {
 			ref = context.content || context.embeddedContentItem
 			if (ref != null) {
@@ -20,58 +19,58 @@ angular.module("umbraco").controller("ImageHotspotController", function($scope, 
 				if (props) {
 					var imageProperties = props.filter(function(prop) { return prop.alias.match(aliasRE) })
 					if (imageProperties.length >= 1) {
-						imageRef = imageProperties[0].value;
-						found = true;
+						imageRef = imageProperties[0].value
+						found = true
 					}
 				}
 			}
-			context = context.$parent;
-			maxRecurse -= 1;
+			context = context.$parent
+			maxRecurse -= 1
 		}
 		
 		if (imageRef != "") {
 			mediaResource.getById(imageRef).then(function(media) {
-				$scope.image.src = media.mediaLink;
+				$scope.image.src = media.mediaLink
 			})
 		}
 	}
 
-	var	$image = $('.imagehotspot-image img', $($element));
+	var	$image = $('.imagehotspot-image img', $($element))
 	
 	$scope.initDragging = function () {
 		$('.imagehotspot-hotspot', $($element)).draggable({
 			cursorAt: { left: 0, top: 0 },
 			drag: function (event, ui) {
-				ui.position.left = Math.max(0, ui.position.left);
-				ui.position.left = Math.min(ui.position.left, $scope.image.width);
-				ui.position.top = Math.max(0, ui.position.top);
-				ui.position.top = Math.min(ui.position.top, $scope.image.height);
+				ui.position.left = Math.max(0, ui.position.left)
+				ui.position.left = Math.min(ui.position.left, $scope.image.width)
+				ui.position.top = Math.max(0, ui.position.top)
+				ui.position.top = Math.min(ui.position.top, $scope.image.height)
 			},
 			stop: function (event, ui) {
-				$scope.storePosition(ui.position.left, ui.position.top);
+				$scope.storePosition(ui.position.left, ui.position.top)
 			}
-		});
+		})
 	}
 	
 	$scope.positionHotspot = function ($event) {
-		var offsetX = $event.offsetX;
-		var offsetY = $event.offsetY;
-		var container = $event.target;
+		var offsetX = $event.offsetX
+		var offsetY = $event.offsetY
+		var container = $event.target
 		
-		var $hotspot = $(".imagehotspot-hotspot", container);
+		var $hotspot = $(".imagehotspot-hotspot", container)
 		$hotspot.css({
 			left: offsetX + "px",
 			top: offsetY + "px"
-		});
+		})
 		
-		$scope.storePosition(offsetX, offsetY);
-	};
+		$scope.storePosition(offsetX, offsetY)
+	}
 	
 	$scope.storePosition = function (x, y) {
-		$scope.assertImageDimensions();
+		$scope.assertImageDimensions()
 		
-		var percentX = 100 * x / $scope.image.width;
-		var percentY = 100 * y / $scope.image.height;
+		var percentX = 100 * x / $scope.image.width
+		var percentY = 100 * y / $scope.image.height
 		
 		$scope.model.value = {
 			image: $scope.image.src,
@@ -81,16 +80,16 @@ angular.module("umbraco").controller("ImageHotspotController", function($scope, 
 			percentY: percentY,
 			width: $scope.image.width,
 			height: $scope.image.height
-		};
+		}
 	}
 	
 	// This should not be called before the image has loaded
 	$scope.assertImageDimensions = function () {
 		if (($scope.image.height === 0) && ($image.height() != null)) {
-			$scope.image.height = $image.height();
+			$scope.image.height = $image.height()
 		}
 	}
 	
-	$scope.setImageSrc($scope, $scope.model.config.imageSrc);
-	$scope.initDragging();
-});
+	$scope.setImageSrc($scope, $scope.model.config.imageSrc)
+	$scope.initDragging()
+})
